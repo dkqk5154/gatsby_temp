d45a543e9ce7acb2d4708521293892932067e685
dkqk5154 <dkqk5154@gmail.com>
2019-05-20 23:23:04 +0900

    first

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>404 Err</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
